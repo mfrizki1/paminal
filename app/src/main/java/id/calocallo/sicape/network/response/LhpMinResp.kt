cb43d27470d8e6c8dafecffc46ab88af23ba0d0a
mfrizki1 <mfrizki1@gmail.com>
2021-03-04 11:26:23 +0700

    add single referensi penyelidikan

---
+++ app/src/main/java/id/calocallo/sicape/network/response/LhpMinResp.kt
@@ -0,0 +1,15 @@
+package id.calocallo.sicape.network.response
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class LhpMinResp(
+    val id: Int?,
+    val no_lhp: String?,
+    val created_at: String?,
+    val updated_at: String?,
+    val deleted_at: String?
+):Parcelable{
+    constructor():this(null,null,null,null,null)
+}
