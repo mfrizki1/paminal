fce9ea4d9396d481a1ce6277558f550d1fa1dc00
mfrizki1 <mfrizki1@gmail.com>
2021-03-06 09:52:31 +0700

    Added SKHD Service ,Change Constant Endpoint LP

--- app/src/main/java/id/calocallo/sicape/network/ApiLp.kt
+++ app/src/main/java/id/calocallo/sicape/network/ApiLp.kt
@@ -8,17 +8,35 @@ import retrofit2.http.*
 interface ApiLp {
     companion object {
         const val ACCEPT = "Accept: application/json"
+
+        const val JENIS_LP = "lp/{jenis_lp}"
+        const val GENERATE_LP = "lp/{id_lp}/generate/document"
+        const val LP_WITH_ID = "lp/{id_lp}"
+        const val LP_PIDANA = "lp/pidana"
+        const val LP_KKE = "lp/kode/etik"
+        const val LP_DISIPLIN = "lp/disiplin"
+
+        const val PASAL = "pasal"
+        const val PASAL_WITH_ID = "pasal/{id_pasal}"
+
+        const val PASAL_DILANGGAR = "lp/{id_pasal}/pasal/dilanggar"
+        const val UPD_PASAL_DILANGGAR = "lp/pasal/dilanggar/{id_pasal_dilanggar}"
+
+        const val SAKSI_LP = "lp/{id_lp}/saksi/kode/etik"
+        const val UPD_SAKSI_LP = "lp/saksi/kode/etik/{id_saksi}"
+
+
     }
 
     @Headers(ACCEPT)
-    @GET("lp/{jenis_lp}")
+    @GET(JENIS_LP)
     fun getLpByJenis(
         @Header("Authorization") tokenBearer: String,
         @Path("jenis_lp") jenis_lp: String
     ): Call<ArrayList<LpMinResp>>
 
     @Headers(ACCEPT)
-    @GET("lp/{id_lp}")
+    @GET(LP_WITH_ID)
     fun getLpById(
         @Header("Authorization") tokenBearer: String,
         @Path("id_lp") id_lp: Int?
@@ -26,42 +44,42 @@ interface ApiLp {
 
 
     @Headers(ACCEPT)
-    @GET("lp/{id_lp}/generate/document")
+    @GET(GENERATE_LP)
     fun generateLp(
         @Header("Authorization") tokenBearer: String,
         @Path("id_lp") id_lp: Int?
     ): Call<Base1Resp<DokLpResp>>
 
     @Headers(ACCEPT)
-    @DELETE("lp/{id_lp}")
+    @DELETE(LP_WITH_ID)
     fun delLp(
         @Header("Authorization") tokenBearer: String,
         @Path("id_lp") id_lp: Int?
     ): Call<BaseResp>
 
     @Headers(ACCEPT)
-    @POST("lp/pidana")
+    @POST(LP_PIDANA)
     fun addLpPidana(
         @Header("Authorization") tokenBearer: String,
         @Body lpPidanaReq: LpPidanaReq
-    ):Call<Base1Resp<DokLpResp>>
+    ): Call<Base1Resp<DokLpResp>>
 
     @Headers(ACCEPT)
-    @POST("lp/disiplin")
+    @POST(LP_DISIPLIN)
     fun addLpDisiplin(
         @Header("Authorization") tokenBearer: String,
         @Body lpDisiplinReq: LpDisiplinReq
-    ):Call<Base1Resp<DokLpResp>>
+    ): Call<Base1Resp<DokLpResp>>
 
     @Headers(ACCEPT)
-    @POST("lp/kode/etik")
+    @POST(LP_KKE)
     fun addLpKke(
         @Header("Authorization") tokenBearer: String,
         @Body lpKkeReq: LpKkeReq
-    ):Call<Base1Resp<DokLpResp>>
+    ): Call<Base1Resp<DokLpResp>>
 
     @Headers(ACCEPT)
-    @PATCH("lp/{id_lp}")
+    @PATCH(LP_WITH_ID)
     fun updLpPidana(
         @Header("Authorization") tokenBearer: String,
         @Path("id_lp") id_lp: Int?,
@@ -69,7 +87,7 @@ interface ApiLp {
     ): Call<Base1Resp<DokLpResp>>
 
     @Headers(ACCEPT)
-    @PATCH("lp/{id_lp}")
+    @PATCH(LP_WITH_ID)
     fun updLpDisiplin(
         @Header("Authorization") tokenBearer: String,
         @Path("id_lp") id_lp: Int?,
@@ -77,7 +95,7 @@ interface ApiLp {
     ): Call<Base1Resp<DokLpResp>>
 
     @Headers(ACCEPT)
-    @PATCH("lp/{id_lp}")
+    @PATCH(LP_WITH_ID)
     fun updLpKke(
         @Header("Authorization") tokenBearer: String,
         @Path("id_lp") id_lp: Int?,
@@ -87,18 +105,18 @@ interface ApiLp {
 
     /*PASAL*/
     @Headers(ACCEPT)
-    @GET("pasal")
+    @GET(PASAL)
     fun getAllPasal(@Header("Authorization") tokenBearer: String): Call<ArrayList<PasalResp>>
 
     @Headers(ACCEPT)
-    @POST("pasal")
+    @POST(PASAL)
     fun addPasal(
         @Header("Authorization") tokenBearer: String,
         @Body pasalReq: PasalReq
     ): Call<Base1Resp<AddPasalResp>>
 
     @Headers(ACCEPT)
-    @PATCH("pasal/{id_pasal}")
+    @PATCH(PASAL_WITH_ID)
     fun updPasal(
         @Header("Authorization") tokenBearer: String,
         @Path("id_pasal") id_pasal: Int,
@@ -106,14 +124,14 @@ interface ApiLp {
     ): Call<Base1Resp<AddPasalResp>>
 
     @Headers(ACCEPT)
-    @GET("pasal/{id_pasal}")
+    @GET(PASAL_WITH_ID)
     fun getPasalById(
         @Header("Authorization") tokenBearer: String,
         @Path("id_pasal") id_pasal: Int
     ): Call<PasalResp>
 
     @Headers(ACCEPT)
-    @DELETE("pasal/{id_pasal}")
+    @DELETE(PASAL_WITH_ID)
     fun delPasal(
         @Header("Authorization") tokenBearer: String,
         @Path("id_pasal") id_pasal: Int
@@ -121,14 +139,14 @@ interface ApiLp {
 
     /*PASAL DILANGGAR*/
     @Headers(ACCEPT)
-    @GET("lp/{id_pasal}/pasal/dilanggar")
+    @GET(PASAL_DILANGGAR)
     fun getPasalDilanggar(
         @Header("Authorization") tokenBearer: String,
         @Path("id_pasal") id_pasal: Int
     ): Call<ArrayList<PasalDilanggarResp>>
 
     @Headers(ACCEPT)
-    @POST("lp/{id_pasal}/pasal/dilanggar")
+    @POST(PASAL_DILANGGAR)
     fun addPasalDilanggar(
         @Header("Authorization") tokenBearer: String,
         @Path("id_pasal") id_pasal: Int,
@@ -136,7 +154,7 @@ interface ApiLp {
     ): Call<Base1Resp<AddPasalDilanggarResp>>
 
     @Headers(ACCEPT)
-    @PATCH("lp/pasal/dilanggar/{id_pasal_dilanggar}}")
+    @PATCH(UPD_PASAL_DILANGGAR)
     fun updPasalDilanggar(
         @Header("Authorization") tokenBearer: String,
         @Path("id_pasal_dilanggar") id_pasal_dilanggar: Int,
@@ -144,7 +162,7 @@ interface ApiLp {
     ): Call<Base1Resp<AddPasalDilanggarResp>>
 
     @Headers(ACCEPT)
-    @DELETE("lp/pasal/dilanggar/{id_pasal_dilanggar}")
+    @DELETE(UPD_PASAL_DILANGGAR)
     fun delPasalDilanggar(
         @Header("Authorization") tokenBearer: String,
         @Path("id_pasal_dilanggar") id_pasal_dilanggar: Int
@@ -153,14 +171,14 @@ interface ApiLp {
     /*SAKSI*/
 
     @Headers(ACCEPT)
-    @GET("lp/{id_lp}/saksi/kode/etik")
+    @GET(SAKSI_LP)
     fun getSaksiByIdLp(
         @Header("Authorization") tokenBearer: String,
         @Path("id_lp") id_lp: Int
     ): Call<ArrayList<LpSaksiResp>>
 
     @Headers(ACCEPT)
-    @POST("lp/{id_lp}/saksi/kode/etik")
+    @POST(SAKSI_LP)
     fun addSaksiByIdLp(
         @Header("Authorization") tokenBearer: String,
         @Path("id_lp") id_lp: Int,
@@ -168,7 +186,7 @@ interface ApiLp {
     ): Call<Base1Resp<AddSaksiResp>>
 
     @Headers(ACCEPT)
-    @PATCH("lp/saksi/kode/etik/{id_saksi}")
+    @PATCH(UPD_SAKSI_LP)
     fun updSaksiSingle(
         @Header("Authorization") tokenBearer: String,
         @Path("id_saksi") id_saksi: Int,
@@ -176,7 +194,7 @@ interface ApiLp {
     ): Call<Base1Resp<AddSaksiResp>>
 
     @Headers(ACCEPT)
-    @DELETE("lp/saksi/kode/etik/{id_saksi}")
+    @DELETE(UPD_SAKSI_LP)
     fun delSaksiSingle(
         @Header("Authorization") tokenBearer: String,
         @Path("id_saksi") id_saksi: Int
